9ce12cb9613b6750723ba22600c684b801600d87
DAK <46497387+DAK404@users.noreply.github.com>
2021-05-31 16:34:07 +0530

    Code update
    Initial implementation of database drivers
    menu logic improvements

--- Source/Truncheon/Core/MainMenu.java
+++ Source/Truncheon/Core/MainMenu.java
@@ -8,6 +8,8 @@ public final class MainMenu
 {
     private byte count = 5;
     private boolean scriptMode = false;
+    private boolean pseudo = false;
+
     public final void mainMenu()throws Exception
     {
         try
@@ -18,6 +20,20 @@ public final class MainMenu
         catch(Exception E)
         {
 
+        }
+    }
+
+
+    private void pseudoLogic()throws Exception
+    {
+        try
+        {
+            System.out.println("Please login Administrator credentials to continue...");
+            //Logic for the login part of the program.
+        }
+        catch(Exception E)
+        {
+            new Truncheon.API.ErrorHandler().handleException(E);
         }
     }
 
@@ -54,9 +70,10 @@ public final class MainMenu
     {
         try
         {
-            Console console=System.console();
             //String[] cmd = input.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
             String[] cmd = input.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            if(cmd.length > 1)
+                cmd[1] = cmd[1].replaceAll("\"", "");
             switch(cmd[0])
             {
                 case "script":
@@ -81,16 +98,16 @@ public final class MainMenu
                 case "?":
                 case "help":
                             System.out.println("Available options:\n");
-                            System.out.println("exit\nrestart\nclear\nhelp\nfeatureX\n\nWhere X is the feature ID");
+                            System.out.println("\nContextual Help\n- Script : Run a script in Truncheon Shell\n- Exit : Exit the program\n- Restart : Restart the program\n- Clear : Clear the screen\n- Help : Open this contextual Help\n- FeatureX : run a feature with X being the ID of the feature");
                             break;
 
                 case "":
                             break;
 
-                case "feature1":
+                case "feature11":
                             throw new Exception();
 
-                case "feature2":
+                case "feature1":
                             System.out.println("SHA3-256 for Hello World! is : " + new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256("Hello World!"));
                             System.out.println("SHA3-256 for File ProgramLauncher.class is : " + new Truncheon.API.Minotaur.HAlgos().fileToSHA3_256("./ProgramLauncher.class"));
 
@@ -98,36 +115,43 @@ public final class MainMenu
                             System.out.println("MD5 for File ProgramLauncher.class is : " + new Truncheon.API.Minotaur.HAlgos().fileToMD5("./ProgramLauncher.class"));
                             break;
 
-                case "feature3":
+                case "feature2":
                             new Truncheon.API.Wraith.WriteFile().editFile("./Trash/");
                             break;
 
-                case "feature4":
+                case "feature3":
                             new Truncheon.API.Wraith.ReadFile().readUserFile("./Trash/");
                             break;
 
-                case "feature5":
+                case "feature12":
                             System.exit(900);
 
-                case "feature6":
+                case "sys":
+                case "feature4":
                             //Run system cmd from Truncheon
                             //pseudo?
                             new ProcessBuilder("cmd", "/c", cmd[1]).inheritIO().start().waitFor();
                             break;
                 
-                case "feature7":
+                case "syshell":
+                case "feature5":
                             new ProcessBuilder("cmd").inheritIO().start().waitFor();
                             break;
 
-                case "feature8":
+                case "echo":
+                case "feature6":
                             if(cmd[1].equalsIgnoreCase(null))
                                 System.out.println("null");
                             else
-                                System.out.println(cmd[1].replaceAll("\"", ""));
+                                System.out.println(cmd[1]);
+                            break;
+
+                case "setup":
+                            new Truncheon.Core.Setup().setupLogic();
                             break;
 
                 default:
-                            System.out.println("Invalid Command.");
+                            System.out.println(input+" - Command not found.");
                             break;
             }
             return;
--- Source/Truncheon/Core/Setup.java
+++ Source/Truncheon/Core/Setup.java
@@ -1,6 +1,11 @@
 package Truncheon.Core;
 
 import java.io.Console;
+import java.io.File;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
 
 public class Setup
 {
@@ -9,6 +14,8 @@ public class Setup
     {
         new Truncheon.API.BuildInfo().versionViewer();
         console.readLine("Welcome to Truncheon! This program needs an initial configuration before using it. To begin the setup, press the Enter/Return key. Else press the Ctrl + C keys.\nWe recommend the Computer Administrator to setup Truncheon. Please contact your Administrator if you are a user and you're seeing this message.");
+        createDirs();
+        initializeDatabase();
     }
 
     private void showPrerequisites()
@@ -18,26 +25,70 @@ public class Setup
 
     private void createDirs()
     {
+        System.out.println("Checking for previous installation and existing directories...");
+        String[] directoryList = {"./System", "./User", "./System/Public", "./System/Private", "./System/Public/Truncheon", "./System/Public/Truncheon/Logs", "./System/Private/Truncheon"};
+        
+        for(int i = 0; i < directoryList.length; i++)
+        {
+            File makeDir =  new File(directoryList[i]);
 
+            if(makeDir.exists() == false)
+                makeDir.mkdirs();
+            else
+                continue;            
+        }
+        return;
     }
 
     private void createFiles()
     {
-
+        //create the M1, M2 and M3 files.
     }
 
     private void initializeDatabase()
     {
+        //TODO : IMPORT THE FSAD TABLE AND IMPORT THE CURRENT DB IF EXISTS
 
+        try 
+        {           
+            //Mud.db = Master User Database file
+            String url = "jdbc:sqlite:./System/Private/Truncheon/mud.db";
+
+            //create a connection to the database
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+
+            System.out.println("Master User Database File created successfully!");
+
+            String sql = "CREATE TABLE IF NOT EXISTS FCAD (\n" +
+                "    UserID integer PRIMARY KEY,\n" +
+				"    Name text NOT NULL,\n" +
+                "    Username text UNIQUE,\n" +
+                "    Password text NOT NULL,\n" +
+                "    SecurityKey text NOT NULL,\n" +
+				"    PIN text NOT NULL,\n" +
+				"    Administrator text NOT NULL);";
+				
+            stmt.execute(sql);
+            System.out.println("Master User Database File has been initialized successfully!");
+        } 
+        catch (Exception E) 
+        {
+            new Truncheon.API.ErrorHandler().handleException(E);
+        } 
+        return;
     }
 
     private void createAdminUser()
     {
 
+
+
     }
 
     private void cleanup()
     {
-        
+        //remove unwanted files out of the directory
     }
 }
