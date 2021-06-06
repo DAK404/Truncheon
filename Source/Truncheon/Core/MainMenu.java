c3b6b9b1b4148dcb556ad7cdea89321e2cd74f01
DAK <46497387+DAK404@users.noreply.github.com>
2021-06-06 14:11:32 +0530

    Code update
    Complete the main menu implementation.
    
    implement minor improvements and error fixes.

--- Source/Truncheon/Core/MainMenu.java
+++ Source/Truncheon/Core/MainMenu.java
@@ -3,6 +3,7 @@ package Truncheon.Core;
 import java.io.Console;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.File;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -26,15 +27,13 @@ public final class MainMenu
     {
         try
         {
-            //login();
             while(login() == false);
             getUserDetails();
             menuShell();
-
         }
         catch(Exception E)
         {
-
+            new Truncheon.API.ErrorHandler().handleException(E);
         }
     }
 
@@ -51,18 +50,10 @@ public final class MainMenu
             String securityKey = new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256(String.valueOf(console.readPassword("Security Key: ")));
             loginStatus = new Truncheon.API.Dragon.LoginAPI(username, password, securityKey).status();
             if(loginStatus==false)
-            {
-                count--;
-                if(count <= 0)
-                {
-                    System.out.println("\n\n[ ERROR ] : Too many requests. Inputs blocked for 10 minutes.");
-                    Thread.sleep(600000);
-                    count = 1;
-                    console.readLine("Login attempt has been rearmed.\nIf you want to quit, press the CTRL + C keys, or press enter to continue.");
-                }
-            }
+                counterLogic();
             password = "";
             securityKey = "";
+            count = 5;
             return loginStatus;
             
         }
@@ -73,17 +64,136 @@ public final class MainMenu
         return false;
     }
 
+    private final void scriptEngine(String fileName)throws Exception
+    {
+        try
+        {
+            if(new File(fileName).exists() == false)
+            {
+                System.out.println("[ ATTENTION ] : Script file "+fileName+".nScript has not been found.\nPlease check the directory of the script file and try again.");
+                return;
+            }
+            scriptMode = true;
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+    
+                String scriptLine;
+                while ((scriptLine = br.readLine()) != null) 
+                {
+                    if(scriptLine.toString().startsWith("#"))
+                        continue;
+                    else if(scriptLine.equalsIgnoreCase("End Script"))
+                        break;
+                    menuLogic(scriptLine.toString());
+                }
+                br.close();
+            scriptMode = false;
+        }
+        catch(Exception E)
+        {
+            new Truncheon.API.ErrorHandler().handleException(E);
+        }
+    }
+
     private final void menuShell()throws Exception
     {
-        //Console console=System.console();
-        /*while(true)
-            menuLogic(console.readLine(name+"@SYSTEM> ").toLowerCase());*/
-
-        System.out.println(name+"@SYSTEM> ");
-        System.out.println("PIN : " + PIN);
-        System.out.println("Username : " + username);
+        new Truncheon.API.BuildInfo().versionViewer();
         System.out.println("Administrator : " + admin);
-            
+        while(true)
+            menuLogic(console.readLine(name+"@SYSTEM> ").toLowerCase());
+    }
+
+    private final void menuLogic(String input)
+    {
+        try
+        {
+            //String[] cmd = input.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            String[] cmd = input.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            if(cmd.length > 1)
+                cmd[1] = cmd[1].replaceAll("\"", "");
+            switch(cmd[0])
+            {
+                //Implementation of the script functionality
+                case "script":
+                    if(cmd.length <= 1)
+                    {
+                        System.out.println("\nScript Syntax:\nscript <script_name/path>\n");
+                        return;
+                    }
+                    if(scriptMode==true)
+                    {
+                        System.out.println("Cannot execute another script while running a script.");
+                        return;
+                    }
+                    scriptEngine("./Users/"+username+"/"+cmd[1] +".nScript");
+                    break;
+
+                case "about":
+                    new Truncheon.API.BuildInfo().about();
+                    break;
+
+                //Exits the program at the user's command
+                case "exit":
+                    System.exit(0);
+
+                //Restarts the program
+                case "restart":
+                    System.exit(1);
+
+                //clears the screen
+                case "clear":
+                    new Truncheon.API.BuildInfo().versionViewer();
+                    break;
+
+                //Invokes the help file... Right now its the help menu
+                case "?":
+                case "help":
+                    System.out.println("Available options:\n");
+                    System.out.println("\nContextual Help\n- Script : Run a script in Truncheon Shell\n- Exit : Exit the program\n- Restart : Restart the program\n- Clear : Clear the screen\n- Help : Open this contextual Help\n- FeatureX : run a feature with X being the ID of the feature");
+                    break;
+
+                //Ignore any empty inputs and pass it back to the shell
+                case "":
+                    break;
+                
+                //Invokes the system shell while running Truncheon
+                case "syshell":
+                    if(System.getProperty("os.name").contains("Windows"))
+                        new ProcessBuilder("cmd").inheritIO().start().waitFor();
+                    else
+                        new ProcessBuilder("/bin/bash", "-c" , cmd[1]).inheritIO().start().waitFor();
+                    break;
+
+                //Runs a system command within Truncheon Shell
+                case "sys":
+                    if(System.getProperty("os.name").contains("Windows"))
+                        new ProcessBuilder("cmd", "/c", cmd[1]).inheritIO().start().waitFor();
+                    else
+                        new ProcessBuilder("/bin/bash", "-c" , cmd[1]).inheritIO().start().waitFor();
+                    break;
+
+                //Prints a line onto the terminal, used mostly in scripting stuff
+                case "echo":
+                    if(cmd[1].equalsIgnoreCase(null))
+                        System.out.println("null");
+                    else
+                        System.out.println(cmd[1]);
+                    break;
+
+                //Implement the Lock console functionality
+                case "lock":
+                    lockConsole();
+                    break;
+
+                default:
+                    System.out.println(input+" - Command not found.");
+                    break;
+            }
+            return;
+        }
+        catch(Exception E)
+        {
+            new Truncheon.API.ErrorHandler().handleException(E);
+        }
     }
 
     private final void getUserDetails()throws Exception
@@ -94,12 +204,10 @@ public final class MainMenu
         if( retrieveInfo("SELECT Administrator FROM FCAD WHERE Username = ? ;", "Administrator").equals("Yes") )
             admin=true;
         System.gc();
-        System.out.println("AAAAA");
     }
 
     private final String retrieveInfo(String command, String info)throws Exception
     {
-        System.out.println("BBBBB");
         String url = "jdbc:sqlite:./System/Private/Truncheon/mud.db";
         Connection conn = DriverManager.getConnection(url);
 
@@ -116,141 +224,18 @@ public final class MainMenu
         System.gc();
         return temp;
     }
-}
 
-    /*private final void pseudoLogic()throws Exception
+    private final void lockConsole()
     {
         try
         {
-            System.out.println("Please login Administrator credentials to continue...");
-            //Logic for the login part of the program.
-        }
-        catch(Exception E)
-        {
-            new Truncheon.API.ErrorHandler().handleException(E);
-        }
-    }*/
-
-    /*private final void scriptEngine(String fileName)throws Exception
-    {
-        scriptMode = true;
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
- 
-            String scriptLine;
-            while ((scriptLine = br.readLine()) != null) 
+            while(! (console.readLine("AFK@IDLE> ").equalsIgnoreCase("unlock console") ));
+            while(! (new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256(console.readLine("Plase Authenticate with the Unlock PIN:\nPIN : ")).equals(PIN)))
             {
-                if(scriptLine.toString().startsWith("#"))
-                    continue;
-                else if(scriptLine.equalsIgnoreCase("End Script"))
-                    break;
-                menuLogic(scriptLine.toString());
+                counterLogic();
             }
-            br.close();
-        scriptMode = false;
-    }
+            new Truncheon.API.BuildInfo().versionViewer();
 
-    
-
-    private final void menuLogic(String input)
-    {
-        try
-        {
-            //String[] cmd = input.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-            String[] cmd = input.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-            if(cmd.length > 1)
-                cmd[1] = cmd[1].replaceAll("\"", "");
-            switch(cmd[0])
-            {
-                case "script":
-                            if(scriptMode==true)
-                            {
-                                System.out.println("Cannot execute another script while running a script.");
-                                return;
-                            }
-                            scriptEngine("./Trash/"+cmd[1] +".nScript");
-                            break;
-
-                case "exit":
-                            System.exit(0);
-
-                case "restart":
-                            System.exit(1);
-
-                case "clear":
-                            new Truncheon.API.BuildInfo().versionViewer();
-                            break;
-
-                case "?":
-                case "help":
-                            System.out.println("Available options:\n");
-                            System.out.println("\nContextual Help\n- Script : Run a script in Truncheon Shell\n- Exit : Exit the program\n- Restart : Restart the program\n- Clear : Clear the screen\n- Help : Open this contextual Help\n- FeatureX : run a feature with X being the ID of the feature");
-                            break;
-
-                case "":
-                            break;
-
-                case "feature11":
-                            throw new Exception();
-
-                case "feature1":
-                            System.out.println("SHA3-256 for Hello World! is : " + new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256("Hello World!"));
-                            System.out.println("SHA3-256 for File ProgramLauncher.class is : " + new Truncheon.API.Minotaur.HAlgos().fileToSHA3_256("./ProgramLauncher.class"));
-
-                            System.out.println("MD5 for Hello World! is : " + new Truncheon.API.Minotaur.HAlgos().stringToMD5("Hello World!"));
-                            System.out.println("MD5 for File ProgramLauncher.class is : " + new Truncheon.API.Minotaur.HAlgos().fileToMD5("./ProgramLauncher.class"));
-                            break;
-
-                case "feature2":
-                            new Truncheon.API.Wraith.WriteFile().editFile("./Trash/");
-                            break;
-
-                case "feature3":
-                            new Truncheon.API.Wraith.ReadFile().readUserFile("./Trash/");
-                            break;
-
-                case "feature12":
-                            System.exit(900);
-
-                case "sys":
-                case "feature4":
-                            //Run system cmd from Truncheon
-                            //pseudo?
-                            new ProcessBuilder("cmd", "/c", cmd[1]).inheritIO().start().waitFor();
-                            break;
-                
-                case "syshell":
-                case "feature5":
-                            new ProcessBuilder("cmd").inheritIO().start().waitFor();
-                            break;
-
-                case "echo":
-                case "feature6":
-                            if(cmd[1].equalsIgnoreCase(null))
-                                System.out.println("null");
-                            else
-                                System.out.println(cmd[1]);
-                            break;
-
-                case "setup":
-                            new Truncheon.Core.Setup().setupLogic();
-                            break;
-
-                case "lock console":
-                            while(console.readLine("AFK@IDLE> ").equalsIgnoreCase("unlock console"));
-                            break;
-
-                case "login":
-                            if(login()==false)
-                                System.out.println("Login Failed");
-                            else
-                                System.out.println("Login success");
-                            break;
-
-
-                default:
-                            System.out.println(input+" - Command not found.");
-                            break;
-            }
             return;
         }
         catch(Exception E)
@@ -259,38 +244,15 @@ public final class MainMenu
         }
     }
 
-    private final boolean login()throws Exception
+    private void counterLogic()throws Exception
     {
-        try
+        count--;
+        if(count <= 0)
         {
-            boolean loginStatus = false;
-            new Truncheon.API.BuildInfo().versionViewer();
-            System.out.println("Login to Continue.\n");
-            System.out.println("Login Attempts Remaining: "+count+"\n\n");
-            String Username = new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256(console.readLine("Username: "));
-            String Password = new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256(String.valueOf(console.readPassword("Password: ")));
-            String SecurityKey = new Truncheon.API.Minotaur.HAlgos().stringToSHA3_256(String.valueOf(console.readPassword("Security Key: ")));
-            loginStatus = new Truncheon.API.Dragon.LoginAPI(Username, Password, SecurityKey).status();
-            if(loginStatus==false)
-            {
-                count--;
-                if(count <= 0)
-                {
-                    System.out.println("\n\n[ ERROR ] : Too many requests. Inputs blocked for 10 minutes.");
-                    Thread.sleep(600000);
-                    count = 1;
-                    console.readLine("Login attempt has been rearmed.\nIf you want to quit, press the CTRL + C keys, or press enter to continue.");
-                }
-            }
-            return loginStatus;
-            
+            System.out.println("\n\n[ ERROR ] : Too many requests. Inputs blocked for 10 minutes.");
+            Thread.sleep(600000);
+            count = 1;
+            console.readLine("Attempt has been rearmed. Please try again.");
         }
-        catch(Exception E)
-        {
-            new Truncheon.API.ErrorHandler().handleException(E);
-        }
-        return false;
     }
 }
-*/
-
