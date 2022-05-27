c0207b9d51fdd0fd6f6c2fc732478d96fe5d351a
DAK <46497387+DAK404@users.noreply.github.com>
2022-05-27 23:59:33 +0530

    Update core components
    Update the code to accommodate the update functionalities with basic commenting.
    
    Added a gitignore entry to ignore the cmd files in the Source directory.

--- Source/Truncheon/Core/Loader.java
+++ Source/Truncheon/Core/Loader.java
@@ -1,10 +1,44 @@
 package Truncheon.Core;
 
+//Import the required Java IO classes
+import java.io.File;
 import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+//Import the required Java NIO classes
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+//Import the required Java Util classes
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+//Import the required Java Net package for URL parsing
+import java.net.URL;
+
 import Truncheon.API.IOStreams;
 import Truncheon.API.BuildInfo;
+
 public class Loader 
 {
+    Console console = System.console();
+
+    public Loader()
+    {
+        try
+        {
+
+        }
+        catch(Exception e)
+        {
+            System.err.println("FATAL ERROR: " + e);
+        }
+    }
+
     public static void main(String[] args)throws Exception
     {
         switch(args[0].toLowerCase())
@@ -38,6 +72,108 @@ public class Loader
         }        
     }
 
+    /*
+    ----------------------------------------------------------------------------------
+    SECTION NAME : REPAIR MODE LOGIC
+    OBJECTIVE    : Provide a shell access to repair the system to a working state.
+    This mode is restricted to a small set of commands which can be used to find
+    the root cause of the kernel integrity failures which prevent booting system
+    to the normal mode.
+    AUTHOR       : Deepak Anil Kumar (@DAK404)
+    ----------------------------------------------------------------------------------
+    */
+    
+    private void repairMode()
+    {
+
+    }
+
+    private void repairCmdProcessor()
+    {
+
+    }
+
+    private void repairOnlineMode()
+    {
+
+    }
+
+    private void downloadBuildFromCloud()
+    {
+
+    }
+
+    private void restoreFromBackup()
+    {
+
+    }
+
+    private void removeExistingBinaries()
+    {
+
+    }
+
+    /*
+    ----------------------------------------------------------------------------------
+    END OF REPAIR MODE LOGIC
+    ----------------------------------------------------------------------------------
+    */
+
+    /*
+    ----------------------------------------------------------------------------------
+    SECTION NAME : ABRAXIS LOGIC
+    OBJECTIVE    : Performs an integrity check on the kernel files to prevent any
+    unauthorized changes made. This will enforce only signed binaries to be loaded,
+    notify any unrecognized files and prevent loading of an unsigned binary file.
+    AUTHOR       : Deepak Anil Kumar (@DAK404)
+    ----------------------------------------------------------------------------------
+    */
+
+    private void abraxisLogic()
+    {
+
+    }
+
+    private void populateFiles()
+    {
+
+    }
+
+    private void checkDirectoryStructure()
+    {
+
+    }
+
+    private void checkFileHash()
+    {
+
+    }
+
+    private void fileIgnoreList()
+    {
+
+    }
+
+    /*
+    ----------------------------------------------------------------------------------
+    END OF ABRAXIS LOGIC
+    ----------------------------------------------------------------------------------
+    */
+
+    /*
+    ----------------------------------------------------------------------------------
+    SECTION NAME : DEBUG LOGIC
+    OBJECTIVE    : These are methods which are used to debug the system during the
+    development of the program. These methods shall be removed once the code shall
+    mature after testing the intended features.
+    Also, do note that the methods shall keep changing over time, therefore, please
+    do not build any APIs or code on top of these methods. The instructions for the
+    same can be seen in the Documentation, which shall be released once the code is
+    completed.
+    AUTHOR       : Deepak Anil Kumar (@DAK404)
+    ----------------------------------------------------------------------------------
+    */
+
     private void _debugPrintStreams()
     {
         IOStreams.println("Hello World!");
@@ -59,4 +195,10 @@ public class Loader
 
         System.out.println("End Of Demo.");
     }
+
+    /*
+    ----------------------------------------------------------------------------------
+    END OF DEBUG LOGIC
+    ----------------------------------------------------------------------------------
+    */
 }
