b0642095ed0731682458e304ad2935611590c308
DAK <46497387+DAK404@users.noreply.github.com>
2022-09-15 16:02:57 +0530

    Code Update: Temper the Steel
    - Created base files for Anvil and NionKernel
    - Improved the AccountCreate.java code; Removed null values (unsafe code structures) and fixed the logic to save the new account credentials to the database
    - Fixed LoginAuth to function as expected; changed from the Conch to Truncheon filepath
    - updated Readme.txt

---
+++ Source/Truncheon/API/Anvil.java
@@ -0,0 +1,28 @@
+package Truncheon.API;
+
+import java.io.File;
+//Scripting featureset
+
+// COMMON COMPONENTS LIKE WAIT, INPUT, ECHO ARE HERE. NO REDUNDANCY
+//ANY EXTRA FEATURES ARE PASSED BACK TO THE PROGRAM AND ARE PROCESSED BY THE CALLING PROGRAM
+public class Anvil
+{
+    private String commonCommands(String command)
+    {
+        String result = "";
+        //split the string into array
+        //load the array into the switch statement
+        switch()
+        {
+            default:
+                //set the input string into the result
+            break;
+        }
+        return result;
+    }
+
+    private void loadScriptFile(String fileName)
+    {
+        new File(fileName).exists();
+    }
+}
---
+++ Source/Truncheon/Core/NionKernel.java
@@ -0,0 +1,31 @@
+package Truncheon.Core;
+
+//Previously known as MainMenu.java
+//Implement all the older functionalities here
+public class NionKernel 
+{
+    public void startNionKernel()
+    {
+
+    }
+
+    private void login()
+    {
+
+    }
+
+    private void checkPrerequisites()
+    {
+
+    }
+
+    private void grinchLogic()
+    {
+
+    }
+
+    public void customBuildInfoViewer()
+    {
+
+    }
+}
