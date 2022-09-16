936825ce221f720e82e509c4a13db3e6fc037489
DAK <46497387+DAK404@users.noreply.github.com>
2022-09-17 05:01:51 +0530

    Code Update: Temper the Steel
    - implemented Anvil functionality
    - added aboutProgram() method in BuildInfo.java
    - removed printing of stacktrace when the auth was unsuccessful
    - cleared redundant debug code from Loader and moved it to NionKernel.java
    - Removed the redundant constructor println statements to improve output statements displayed during runtime, removed the default debug strings
    - Fixed a memory leak in Loader.java
    - Implemented the core functionality of NionKernel.
    - Improved performance and optimized the program code

--- Source/Truncheon/API/BuildInfo.java
+++ Source/Truncheon/API/BuildInfo.java
@@ -29,7 +29,31 @@ public class BuildInfo
             System.out.println(" Version    : " + _Version);
             System.out.println(" Build Date : " + _BuildDate);
             System.out.println(" Build ID   : " + _BuildID);
-            System.out.println(" CAUTION! " + _BuildType + " build!\n Expect changes and crashes.");
+            System.out.println("\n CAUTION! " + _BuildType + " build!\n Expect changes and crashes.");
+            System.out.println("---------------------------------------------\n");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void aboutProgram()
+    {
+        try
+        {
+            clearScreen();
+            System.out.println(_Branding);
+            System.out.println("Nion: Truncheon");
+
+            System.out.println("\nAn easy to use, cross-platform shell written in Java\nfor the ease of use and convenience for all users!");
+            
+            System.out.println("\nBuild Version    : " + _Version);
+            System.out.println("Version Codename : " + _VersionCodeName);
+            System.out.println("Build Date       : " + _BuildDate);
+            System.out.println("Build Identifier : " + _BuildID);
+            System.out.println("Build Type/Branch: " + _BuildType);
+            System.out.println("Kernel Name      : " + _KernelName);
             System.out.println("---------------------------------------------\n");
         }
         catch(Exception e)
--- Source/Truncheon/Core/Loader.java
+++ Source/Truncheon/Core/Loader.java
@@ -59,27 +59,15 @@ import Truncheon.API.ExceptionHandler;
 */
 public class Loader
 {
-
+    
     private Console console = System.console();
-
+    
     private static List<String> filePath = new  ArrayList<String>();
-
-    public Loader()
-    {
-        try
-        {
-            System.out.println("Nion: Truncheon");
-            System.out.println("Booting Program. Please wait...");
-        }
-        catch(Exception e)
-        {
-            System.err.println("FATAL ERROR: " + e);
-            System.exit(4);
-        }
-    }
-
+    
     public static void main(String[] args)throws Exception
     {
+        BuildInfo.viewBuildInfo();
+        
         switch(args[0].toLowerCase())
         {
             case "normal":
@@ -97,7 +85,6 @@ public class Loader
         }
 
         System.gc();
-        BuildInfo.viewBuildInfo();
         new Loader().loaderLogic();
     }
 
@@ -129,31 +116,26 @@ public class Loader
                 new Setup().setupLogic();
                 break;
             }
-
-            System.out.println("DEFAULT");
             
+            System.out.println();
+
+            //Start a limited shell here.
             String tempInput = "";
             do
             {
-                BuildInfo.viewBuildInfo();
-                debug();
-                tempInput = console.readLine("~X> ");
+                tempInput = console.readLine("~lounge> ");
                 
-                switch(tempInput)
+                switch(tempInput.toLowerCase())
                 {
-                    case "usermgmt add":
-                        new Truncheon.API.Dragon.AccountCreate().AccountCreateLogic(new Truncheon.API.Minotaur.Cryptography().stringToSHA3_256("Administrator"));
-                        break;
-
-                    case "clear":
-                        BuildInfo.viewBuildInfo();
-                        break;
-
                     case "login":
-                        System.out.println(new Truncheon.API.Dragon.LoginAuth(new Truncheon.API.Minotaur.Cryptography().stringToSHA3_256(console.readLine("Username: "))).authenticationLogic(new Truncheon.API.Minotaur.Cryptography().stringToSHA3_256(console.readLine("Password: ")), new Truncheon.API.Minotaur.Cryptography().stringToSHA3_256(console.readLine("Key: "))));
-                        console.readLine();
-                        break;
+                        new Truncheon.Core.NionKernel().startNionKernel();
+                    break;
+
+                    case "mem":
+                        debug();
+                    break;
                 }
+                System.gc();
             }
             while(! tempInput.equalsIgnoreCase("exit"));
         }
@@ -244,7 +226,7 @@ public class Loader
             //Set the return value to be 2, denoting that the Manifest File is missing
             abraxisResult = 2;
         }
-
+        System.gc();
         return abraxisResult;
     }
 
@@ -334,7 +316,8 @@ public class Loader
             IOStreams.printError("Manifest File Population Failure.");
             kernelIntegrity = false;
         }
-
+        filePath.clear();
+        System.gc();
         return kernelIntegrity;
     }
 
@@ -357,6 +340,7 @@ public class Loader
                 break;
             }
         }
+        System.gc();
         return status;
     }
 
@@ -422,6 +406,7 @@ public class Loader
         new Truncheon.API.Wraith.WraithRead();
         new Truncheon.API.Wraith.WraithEdit();
         new Truncheon.Core.NionKernel();
+        new Truncheon.API.Anvil();
     }
 
     /*
